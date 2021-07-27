12f374c51d7252c5323d1db8635292f518b14a67
unknown <nayefkanaan03@gmail.com>
2021-07-28 02:12:15 +0300

    Added onload method to load css and html then run JS functions

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+    timeColor();
+    time1();
+    time2();
+    time3();
+    time4();
+    time5();
+    time6();
+    time7();
+    time8();
+    time9();
+   }
+   
