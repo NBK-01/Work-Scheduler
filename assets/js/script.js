8fb9688e4975c68980989ada943b6ccff8ba61e5
unknown <nayefkanaan03@gmail.com>
2021-07-28 02:40:55 +0300

    Color coding function written using moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,5 +26,70 @@ var fifteen = $("#text7")
 var sixteen = $("#text8")
 var seventeen = $("#text9")
 
+var now = new Date().getHours();
 
-
+function timeColor() {
+  if (now > 9) {
+    nine.addClass("past");
+  } else if (now >= 9 && now < 10) {
+    nine.addClass("present");
+  } else if (now < 9) {
+    nine.addClass("future");
+  }
+  if (now > 10) {
+    ten.addClass("past");
+  } else if (now >= 10 && now < 11) {
+    ten.addClass("present");
+  } else if (now < 10) {
+    ten.addClass("future");
+  }
+  if (now > 11) {
+    eleven.addClass("past");
+  } else if (now >= 11 && now < 12) {
+    eleven.addClass("present");
+  } else if (now < 11) {
+    eleven.addClass("future");
+  }
+  if (now > 12) {
+    twelve.addClass("past");
+  } else if (now >= 12 && now < 13) {
+    twelve.addClass("present");
+  } else if (now < 12) {
+    twelve.addClass("future");
+  }
+  if (now > 13) {
+    thirteen.addClass("past");
+  } else if (now >= 13 && now < 14) {
+    thirteen.addClass("present");
+  } else if (now < 13) {
+    thirteen.addClass("future");
+  }
+  if (now > 14) {
+    fourteen.addClass("past");
+  } else if (now >= 14 && now < 15) {
+    fourteen.addClass("present");
+  } else if (now < 14) {
+    fourteen.addClass("future");
+  }
+  if (now > 15) {
+    fifteen.addClass("past");
+  } else if (now >= 15 && now < 16) {
+    fifteen.addClass("present");
+  } else if (now < 15) {
+    fifteen.addClass("future");
+  }
+  if (now > 16) {
+    sixteen.addClass("past");
+  } else if (now >= 16 && now < 17) {
+    sixteen.addClass("present");
+  } else if (now < 16) {
+    sixteen.addClass("future");
+  }
+  if (now > 17) {
+    seventeen.addClass("past");
+  } else if (now >= 17 && now < 18) {
+    seventeen.addClass("present");
+  } else if (now < 17) {
+    seventeen.addClass("future");
+  }
+}
